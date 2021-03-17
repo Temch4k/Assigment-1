1a47c0a368a40294e54d894bdb1022f865e6d63f
mstovy <micahstovall@Micahs-MacBook-Pro-2.local>
2021-03-17 14:56:33 -0600

    dax changes

--- JS/main.js
+++ JS/main.js
@@ -4,24 +4,27 @@ function validateForm() {
     var password = document.querySelector("#txtPW");
     var confirmPassword = document.querySelector("#txtPW2");
     var passwordError = document.querySelector("#passwordError")
+    var passwordHasError = false;
 
     if (password.value != confirmPassword.value) {
         formIsValid = false;
         passwordError.innerHTML = "Passwords must match."
         passwordError.classList.remove("invisible");
+        passwordHasError = true
     }
     else {
         passwordError.innerHTML = ""
         passwordError.classList.add("invisible")
     }
     var elements = document.getElementsByTagName("input");
-    var invalidChars = ['<', '>', '#', '{', '}', '(', ')', '!', '~', '"', "'", ''];
+    var invalidChars = ['<', '>', '#', '{', '}', '(', ')', '!', '~', '"', "'", '`'];
 
     for (let i = 0; i < elements.length; i++) {
         for (let j = 0; j < invalidChars.length; j++) {
             if (elements[i].value.indexOf(invalidChars[j]) != -1) {
                 elements[i].classList.add("hasError");
                 formIsValid = false;
+                passwordHasError = true;
                 var divInvalidChar = document.querySelector("#divInvalidCharError");
                 divInvalidChar.classList.remove("invisible");
                 divInvalidChar.innerHTML = "You have entered an invalid char(#, !, ~, {, }, (, ), ', ) in the form. Please remove the char and try again."
@@ -30,9 +33,13 @@ function validateForm() {
     }
     if (checkPassword(password) != true) {
         passwordError.innerHTML = "Your password must include one number, one uppercase and one lowercase letter";
-        formIsValid = false
+        formIsValid = false;
+        passwordHasError = true;
     }
-
+    if(!passwordHasError){
+        passwordError.innerHTML = ""
+        passwordError.classList.add("invisible")
+    }
     return formIsValid;
 }
 function checkPassword(inputText) {
